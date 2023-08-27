4c8a9118e5fd3103df286e89606a7806500fe0ab
makoto hata <makotohata0429@gmail.com>
2023-08-27 11:10:41 +0900

    最初のコミット

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  
+  <!-- <TopPage /> -->
+  <!-- <ProfilePage /> -->
+  <SkillsPage />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TopPage from './components/TopPage.vue'
+// import ProfilePage from './components/ProfilePage.vue'
+import SkillsPage from './components/SkillsPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    // TopPage,
+    // ProfilePage,
+    SkillsPage,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: PT Sans Caption, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333333;
+  background-image: url("./assets/img/bgImage.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: calc(100vw * 9/16);
+
 }
 </style>
