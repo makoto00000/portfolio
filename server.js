9fd65f864884dad0ef15bafbff504ffae7a175b8
makoto hata <makotohata0429@gmail.com>
2023-09-26 11:47:29 +0900

    add rejectUnauthorized

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
-  }
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 app.post("/api/email", async (req,res) => {
